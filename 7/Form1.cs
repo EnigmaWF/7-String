b37afc2c8b1b7435a96fd66c736e20f76c7273df
EnigmaWF <crazyfox.p@mail.ru>
2020-09-08 14:43:11 +0300

    Добавьте файлы проекта.

---
+++ 7/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string t = Convert.ToString(textBox1.Text); // ввод строки
+            string n = "";
+
+            for (int i = 0; i < t.Length; i++)
+            {
+
+                if (t[i] == 't')
+                    n+="t";
+            }
+
+            label1.Text = "" + n;
+            /*Дана строка. Напечатать все её буквы t.*/
+        }
+    }
+}
